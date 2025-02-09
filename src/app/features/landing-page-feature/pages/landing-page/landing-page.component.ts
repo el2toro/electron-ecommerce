431fea3ea3aeea7d2308b705681b086cfee026ea
el2toro <vasea.badragan@gmail.com>
2025-02-09 22:32:47 +0100

    add first banner

--- src/app/features/landing-page-feature/pages/landing-page/landing-page.component.ts
+++ src/app/features/landing-page-feature/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroComponent } from "../../components/hero/hero.component";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.scss']
+  styleUrls: ['./landing-page.component.scss'],
+  imports: [HeroComponent]
 })
 export class LandingPageComponent implements OnInit {
 
