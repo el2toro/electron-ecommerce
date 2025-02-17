223741366a573dc46ce9eaaff98cb67c92dd7577
el2toro <vasea.badragan@gmail.com>
2025-02-17 21:10:53 +0100

    Implement features section

--- src/app/features/landing-page-feature/components/features/features.component.ts
+++ src/app/features/landing-page-feature/components/features/features.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
-  styleUrls: ['./features.component.scss']
+  styleUrls: ['./features.component.scss'],
+  imports: [CommonModule]
 })
 export class FeaturesComponent implements OnInit {
+  offers = ["offer1", "offer2", "offer3", "offer4", "offer5", "offer6", "offer7", "offer8"];
 
   constructor() { }
 
