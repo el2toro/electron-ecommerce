05a44ab51f492aa88d9c4d1468c0565deca9ff9d
el2toro <vasea.badragan@gmail.com>
2025-02-17 22:25:28 +0100

    Add banner

--- src/app/features/landing-page-feature/pages/landing-page/landing-page.component.ts
+++ src/app/features/landing-page-feature/pages/landing-page/landing-page.component.ts
@@ -4,12 +4,13 @@ import { FlashSaleSectionComponent } from "../../components/flash-sale-section/f
 import { BrandsComponent } from "../../components/brands/brands.component";
 import { NewProductComponent } from "../../components/new-product/new-product.component";
 import { FeaturesComponent } from "../../components/features/features.component";
+import { BannerComponent } from "../../components/banner/banner.component";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  imports: [HeroComponent, FlashSaleSectionComponent, BrandsComponent, NewProductComponent, FeaturesComponent]
+  imports: [HeroComponent, FlashSaleSectionComponent, BrandsComponent, NewProductComponent, FeaturesComponent, BannerComponent]
 })
 export class LandingPageComponent implements OnInit {
 
