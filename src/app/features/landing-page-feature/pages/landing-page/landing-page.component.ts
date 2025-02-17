ce6c01c486ba62281fa633b777de096d35b56c0a
el2toro <vasea.badragan@gmail.com>
2025-02-17 19:59:03 +0100

    Create features component

--- src/app/features/landing-page-feature/pages/landing-page/landing-page.component.ts
+++ src/app/features/landing-page-feature/pages/landing-page/landing-page.component.ts
@@ -3,12 +3,13 @@ import { HeroComponent } from "../../components/hero/hero.component";
 import { FlashSaleSectionComponent } from "../../components/flash-sale-section/flash-sale-section.component";
 import { BrandsComponent } from "../../components/brands/brands.component";
 import { NewProductComponent } from "../../components/new-product/new-product.component";
+import { FeaturesComponent } from "../../components/features/features.component";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss'],
-  imports: [HeroComponent, FlashSaleSectionComponent, BrandsComponent, NewProductComponent]
+  imports: [HeroComponent, FlashSaleSectionComponent, BrandsComponent, NewProductComponent, FeaturesComponent]
 })
 export class LandingPageComponent implements OnInit {
 
